3b6127d19d431c857399c911bf3abdfe77e6bf62
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-24 21:00:48 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import EyesFollow from './components/EyesFollow';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Test of npm package: react-hook-mighty-mouse </h1>
+			<EyesFollow/>
+		</div>
+	);
+}
+
+export default App;
